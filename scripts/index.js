722912da61527cad897fff82d6d8257bf84d3ed0
Sergey Ushin <serguei.yushin@yandex.ru>
2024-02-14 11:55:15 +0500

    cards2

--- scripts/index.js
+++ scripts/index.js
@@ -8,35 +8,33 @@
 
 // @todo: Вывести карточки на страницу
 
-function getCards(name, link) {
-  const cardsTemplate = document.querySelector('#card-template').content;
+function createCardElement(name, link, onDelete) {
+  const cardsTemplate = document.querySelector("#card-template").content;
 
-  const cardsElement = cardsTemplate.querySelector('.card').cloneNode(true);
+  const cardsElement = cardsTemplate.querySelector(".card").cloneNode(true);
 
-  cardsElement.querySelector('.card__image').src = link;
-  cardsElement.querySelector('.card__title').textContent = name;
+  const deleteButton = cardsElement.querySelector(".card__delete-button");
 
-  deleteCards(cardsElement);
+  cardsElement.querySelector(".card__image").src = link;
+  cardsElement.querySelector(".card__image").alt = name;
+  cardsElement.querySelector(".card__title").textContent = name;
 
+  deleteButton.addEventListener("click", onDelete);
   return cardsElement;
-};
+}
 
-function deleteCards(cardsElement) {
-
- const deleteButton = cardsElement.querySelector('.card__delete-button');
-  deleteButton.addEventListener('click', function() {
-    cardsElement.remove();
-  });
+function deleteCard(evt) {
+  const deleteButton = evt.target.closest(".card");
+  deleteButton.remove();
 }
 
 function appCards() {
-    const cardsSelector = document.querySelector('.places__list');
+  const cardsSelector = document.querySelector(".places__list");
 
-    initialCards.forEach(function(card) {
-        const newCard = getCards(card.name, card.link);
-        cardsSelector.append(newCard);
-      });
-};
+  initialCards.forEach(function (card) {
+    const newCard = createCardElement(card.name, card.link, deleteCard);
+    cardsSelector.append(newCard);
+  });
+}
 
 appCards();
-
